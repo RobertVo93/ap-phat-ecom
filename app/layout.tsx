4b306319eb02d3159bb7f762eb642e690c9731b9
Robert Vo <robertvo.developer.93@gmail.com>
2025-06-17 20:10:17 +0700

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/app-logo.png" type="image/png" />
+      </head>
       <body className={inter.className}>
         <LanguageProvider>
           <AuthProvider>
