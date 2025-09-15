52c823bce73bb3055747c528b479ea25af81895b
Robert Vo <robertvo.developer.93@gmail.com>
2025-09-15 16:07:05 +0700

    feat: setup multi-zone. #Refs AP-79

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const { ADMIN_URL, POS_URL } = process.env;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -8,6 +9,34 @@ const nextConfig = {
   webpack: (config) => {
     config.cache = false; // Disable caching to prevent the error
     return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/admin",
+        destination: `${ADMIN_URL}`,
+      },
+      {
+        source: "/admin/:path+",
+        destination: `${ADMIN_URL}/:path+`,
+      },
+      {
+        source: "/admin-static/_next/:path+",
+        destination: `${ADMIN_URL}/admin-static/_next/:path+`,
+      },
+      {
+        source: "/pos",
+        destination: `${POS_URL}`,
+      },
+      {
+        source: "/pos/:path+",
+        destination: `${POS_URL}/:path+`,
+      },
+      {
+        source: "/pos-static/_next/:path+",
+        destination: `${POS_URL}/pos-static/_next/:path+`,
+      },
+    ];
   },
 };
 
