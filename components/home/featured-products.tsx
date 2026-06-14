11e37aff311ff6cedc56cd32e1f036e0bd035eab
Robert Vo <robertvo.developer.93@gmail.com>
2026-06-14 20:03:42 +0700

    refactor layout for category and fetured products

--- components/home/featured-products.tsx
+++ components/home/featured-products.tsx
@@ -45,7 +45,7 @@ export function FeaturedProducts({featuredProducts}: Props) {
           </Button>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 mb-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
           {featuredProducts.map((product, index) => (
             <div
               key={product.id}
