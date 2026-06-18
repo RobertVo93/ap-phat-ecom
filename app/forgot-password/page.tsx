f5951ba4b7033898ca859ceec6394ac1a92b5bef
Robert Vo <robertvo.developer.93@gmail.com>
2026-06-18 13:01:02 +0700

    feat: implement forgot password. #Refs AP-119

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+import { ForgotPasswordForm } from "@/components/auth/forgot-password-form";
+
+export default function ForgotPasswordPage() {
+  return (
+    <div className="bg-[#f8f5f0] flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <ForgotPasswordForm />
+    </div>
+  );
+}
